c41fc4f0a3e9d6474fad46830b9f225c13f67991
sanjeev <sanjeevsingh.77d@gmail.com>
2022-12-11 03:09:38 +0530

    Create Dictionary & added

---
+++ CollectionDemoProblems/CollectionDemoProblems/DemoClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemoProblems
+{
+    public class DemoClass
+    {
+        public void DictionaryDemo()
+        {
+            Console.WriteLine("\n**********Dictionary PROGRAM **********\n");
+
+            Dictionary<int, string> objDictionary = new Dictionary<int, string>();
+            objDictionary.Add(1, "SANJEEV");
+            objDictionary.Add(2, "MAHESH");
+            objDictionary.Add(3, "PIYUSH");
+            objDictionary.Add(4, "RAM");
+            objDictionary.Add(5, "RAHUL");
+
+
+
+            Console.WriteLine("\nAccess value using key(key=1): " + objDictionary[1]);
+
+            Console.WriteLine("\nIterating Dictionary: ");
+            foreach (var element in objDictionary)
+            {
+                Console.WriteLine($"Key = {element.Key} , Value = {element.Value}");
+            }
+        }
+    }
+}
---
+++ CollectionDemoProblems/CollectionDemoProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionDemoProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======================  WELCOME COLLECTION PROBLEMS  ==========================");
+
+            Console.WriteLine("Welcome to Collection Demo Classes");
+            DemoClass obj = new DemoClass();
+            obj.DictionaryDemo();
+        }
+    }
+}
